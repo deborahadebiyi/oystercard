bd6b2cf88843d299a5eec3b3018221dc0ff7f547
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-10-19 15:19:17 +0100

    Challenge4: Adding balance

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,3 +1,13 @@
-RSpec.describe Oystercard do
-  
+require 'oyster'
+
+describe Oystercard do
+
+  it "#balance" do
+    oystercard = Oystercard.new
+    expect(subject).to respond_to(:balance)
+  end
+
+  it "has a balance that is equal to 0" do
+    expect(subject.balance).to eq (0)
+  end
 end

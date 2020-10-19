52aaebacd40c6401aa515945b1196e21437b3d94
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-10-19 15:43:06 +0100

    Challenge5: Topup method

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -4,6 +4,11 @@ attr_reader :balance
 
 def initialize
   @balance = 0
+  @amount
+end
+
+def top_up(amount)
+ @balance += amount
 end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -10,4 +10,18 @@ describe Oystercard do
   it "has a balance that is equal to 0" do
     expect(subject.balance).to eq (0)
   end
+
+  it "#top_up" do
+    expect(subject).to respond_to(:top_up)
+  end
+
+  it "#top_up takes one argument" do
+    expect(subject).to respond_to(:top_up).with(1).arguments
+  end
+
+  it "updates the amount" do
+    oystercard = Oystercard.new
+    oystercard.top_up(5)
+    expect(subject.top_up(5)).to eq(5)
+  end
 end
